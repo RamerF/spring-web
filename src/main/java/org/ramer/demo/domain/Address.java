265efbcba1ea0bacf43a35e8aeb6158620af5c6e
ramer <1390635973@qq.com>
2017-11-15 13:22:19 +0800

    Demo: using Thymeleaf.

--- src/main/java/org/ramer/demo/domain/Address.java
+++ src/main/java/org/ramer/demo/domain/Address.java
@@ -5,38 +5,11 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(exclude = { "user" })
+@ToString(exclude = { "user" })
 public class Address{
     private Integer id;
     private String location;
 
     private User user;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        if (!super.equals(o))
-            return false;
-
-        Address address = (Address) o;
-
-        if (id != null ? !id.equals(address.id) : address.id != null)
-            return false;
-        return location != null ? location.equals(address.location) : address.location == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (id != null ? id.hashCode() : 0);
-        result = 31 * result + (location != null ? location.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" + "id=" + id + ", location='" + location + '\'' + '}';
-    }
 }
