3708187ced9a93c0150d76962aee3ab0510346b9
ramer <1390635973@qq.com>
2020-05-24 22:00:29 +0800

    新增: 网关.

--- demo-account/src/main/java/io/github/ramerf/account/controller/AccountController.java
+++ demo-account/src/main/java/io/github/ramerf/account/controller/AccountController.java
@@ -1,7 +1,5 @@
 package io.github.ramerf.account.controller;
 
-import io.github.ramerf.account.entity.Account;
-import io.github.ramerf.account.entity.Account.Gender;
 import io.github.ramerf.wind.core.entity.response.Rs;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +10,11 @@ import org.springframework.web.bind.annotation.*;
  * @since 2020/5/17
  */
 @Slf4j
-@RequestMapping("/account")
 @RestController
+@RequestMapping("/account")
 public class AccountController {
-  @GetMapping("/enumDemo1")
-  public ResponseEntity<Rs<Object>> enumDemo1(@RequestParam("gender") Gender gender) {
-    log.info("enumDemo1:[{}]", gender);
-    return Rs.ok();
-  }
-
-  @GetMapping("/enumDemo2")
-  public ResponseEntity<Rs<Object>> enumDemo2(Account account) {
-    log.info("enumDemo2:[{}]", account.getGender());
-    return Rs.ok();
+  @GetMapping("/halo")
+  public ResponseEntity<Rs<String>> halo() {
+    return Rs.ok("halo");
   }
 }
